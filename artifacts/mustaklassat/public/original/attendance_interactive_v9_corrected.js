fde9791f2e7f6bb7feca0ee13183020735741296
fikrimamdouh <140603050+fikrimamdouh@users.noreply.github.com>
2026-06-22 08:53:48 +0300

    Update attendance_interactive_v9_corrected.js

--- artifacts/mustaklassat/public/original/attendance_interactive_v9_corrected.js
+++ artifacts/mustaklassat/public/original/attendance_interactive_v9_corrected.js
@@ -817,8 +817,8 @@ Object.keys(statusCounts).forEach(statusKey => {
                 emp.absencePenalty = fine;
                 emp.totalFine = totalFine;
                 emp.netSalary = netSalary;
-                saveAttendanceData(attendanceData);
-
+// ممنوع الحفظ داخل renderTables
+// saveAttendanceData(attendanceData);
 // --- START: التعديل المطلوب لحساب التكلفة الصحيحة ---
 deptTotalCost += extractBaseSalary; // اجمع التكلفة المحسوبة حسب أيام المستخلص
 // --- END: التعديل المطلوب ---
