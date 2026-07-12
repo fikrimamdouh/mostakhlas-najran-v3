13c1c3d89de6bb01b23df6c26c9d01970cb99fcf
fikrimamdouh <140603050+fikrimamdouh@users.noreply.github.com>
2026-07-12 12:51:31 +0300

    feat(visits): register visit master schema

--- lib/db/src/index.ts
+++ lib/db/src/index.ts
@@ -1,6 +1,7 @@
 import { drizzle } from "drizzle-orm/node-postgres";
 import pg from "pg";
-import * as schema from "./schema";
+import * as coreSchema from "./schema";
+import * as visitSchema from "./visit-schema";
 
 const { Pool } = pg;
 
@@ -10,7 +11,10 @@ if (!process.env.DATABASE_URL) {
   );
 }
 
+const schema = { ...coreSchema, ...visitSchema };
+
 export const pool = new Pool({ connectionString: process.env.DATABASE_URL });
 export const db = drizzle(pool, { schema });
 
 export * from "./schema";
+export * from "./visit-schema";
